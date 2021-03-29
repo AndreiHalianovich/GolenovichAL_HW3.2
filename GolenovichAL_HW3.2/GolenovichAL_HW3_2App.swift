d8ea3f84cd1b5a1b419f32ae521f347142475dda
Andrei Halianovich <igolenovichbox@gmail.com>
2021-03-29 18:25:42 +0300

    Initial Commit

---
+++ GolenovichAL_HW3.2/GolenovichAL_HW3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  GolenovichAL_HW3_2App.swift
+//  GolenovichAL_HW3.2
+//
+//  Created by Andrei Halianovich on 29.03.21.
+//
+
+import SwiftUI
+
+@main
+struct GolenovichAL_HW3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
