0843c20f3c83c7936c3c99004a4b30fd5367335e
Andrei Halianovich <igolenovichbox@gmail.com>
2021-03-30 23:03:08 +0300

    GolenovichAL_HW3.2

---
+++ GolenovichAL_HW3.2/ColoredSlider.swift
@@ -0,0 +1,53 @@
+//
+//  ColoredSlider.swift
+//  GolenovichAL_HW3.2
+//
+//  Created by Andrei Halianovich on 30.03.21.
+//
+
+import SwiftUI
+
+struct ColoredSlider: View {
+    
+    @Binding var value: Double
+    
+    @State private var text = ""
+    @State private var alertPresented = false
+    
+    var minimumTrackTintColor: Color
+    
+    var body: some View {
+        HStack {
+            Text("\(lround(value))")
+                .foregroundColor(.white)
+                .frame(width: 35)
+            
+            Slider(value: $value, in: 0...255, step: 1)
+                .accentColor(minimumTrackTintColor)
+            
+            TextField("", text: $text,  onCommit: {
+                checkValueFromTextField()
+            })
+            .alert(isPresented: $alertPresented, content: {
+                Alert(title: Text("Wrong format!"), message: Text("Enter a value between 0 and 255"))
+            })
+            .frame(width: 45, alignment: .center)
+            .onReceive(text.publisher.collect()) {
+                self.text = String($0.prefix(3))
+            }
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+        .padding(.horizontal)
+    }
+    
+    private func checkValueFromTextField() {
+        if let value = Double(text), 0...255 ~= value {
+            self.value = value
+        } else {
+            text = String(lround(value))
+            alertPresented.toggle()
+        }
+        
+    }
+}
+
--- GolenovichAL_HW3.2/ContentView.swift
+++ GolenovichAL_HW3.2/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var redColorValue = 30.0
+    @State private var greenColorValue = 120.0
+    @State private var blueColorValue = 230.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(#colorLiteral(red: 0.1183417514, green: 0.3233708143, blue: 0.5748734474, alpha: 1))
+                .ignoresSafeArea()
+            VStack(spacing: 50) {
+                Rectangle()
+                    .foregroundColor(Color(red: redColorValue / 255, green: greenColorValue / 255, blue: blueColorValue / 255))
+                    .frame(height: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .overlay(RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.white, lineWidth: 3))
+                    .padding()
+                VStack (spacing: 15){
+                    ColoredSlider(value: $redColorValue, minimumTrackTintColor: .red)
+                    ColoredSlider(value: $greenColorValue, minimumTrackTintColor: .green)
+                    ColoredSlider(value: $blueColorValue,minimumTrackTintColor: .blue)
+                }
+                Spacer()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 11 Pro")
     }
 }
+
+
